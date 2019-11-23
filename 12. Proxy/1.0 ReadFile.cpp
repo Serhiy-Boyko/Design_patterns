897495f5b9aa668a8bafb25a10596e9243c26fbe
Serhiy Boyko <Serhiy@ukr.net>
2019-11-23 23:59:49 +0200

    Proxy

---
+++ 12. Proxy/1.0 ReadFile.cpp
@@ -0,0 +1,115 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <conio.h>
+
+// ReadFile читає і виводить на екран весь файл
+// ProxyReadFile читає і виводить на екран файл по одній сторінці
+
+class IReadFile {
+protected:
+	virtual char *getLine() = 0;
+public:
+	virtual void AssignFile(std::string filename) = 0;
+	virtual void PrintFile() = 0;
+	virtual ~IReadFile() {};
+};
+
+
+class ReadFile : public IReadFile {
+protected:
+	std::ifstream f;
+	char *str = nullptr;
+	size_t fileSize;
+
+	virtual char *getLine() override {
+		f.getline(str, fileSize);
+		return str;
+	}
+public:
+	virtual void AssignFile(std::string filename) override {
+		delete str;
+		f.open(filename);
+		f.seekg(0, SEEK_END);
+		fileSize = f.tellg();
+		str = new char[fileSize + 1];
+		f.seekg(0);
+	}
+
+	ReadFile(std::string filename) {
+		AssignFile(filename);
+	}
+
+	virtual void PrintFile() override {
+		while (!f.eof())
+			std::cout << getLine() << std::endl;
+		f.seekg(0);
+	}
+
+	virtual ~ReadFile() {
+		delete str;
+		f.close();
+	};
+
+	friend class ProxyReadFile;
+};
+
+class ProxyReadFile : public IReadFile {
+	ReadFile *RF = nullptr;
+	std::string filename;
+
+	virtual char *getLine() override {
+		return RF->getLine();
+	}
+public:
+	virtual void AssignFile(std::string fname) override {
+		delete RF;
+		filename = fname;
+	}
+
+	ProxyReadFile(std::string fname) : filename(fname) { }
+
+	virtual void PrintFile() override {
+		if (!RF)
+			RF = new ReadFile(filename);
+		char *s = nullptr;
+		size_t cnt = 0;
+		int num_of_page = 1;
+		// 120 - count of char in string on screen
+		const size_t nchar = 120;
+		while (!RF->f.eof()) {
+			s = getLine();
+			
+			if (cnt + strlen(s) / nchar + 1 < 29) {
+				cnt += strlen(s) / nchar + 1;
+				std::cout << s << std::endl;
+			}
+			else {
+				std::cout << "------------------ Number of page: ";
+				std::cout << num_of_page++ << " ------" << std::endl;
+				if (_getch() == 27) { // Escape
+					RF->f.seekg(0);
+					std::cout << "End of printting..." << std::endl;
+					break;
+				}
+				else {					
+					cnt = strlen(s) / nchar + 1;
+					std::cout << s << std::endl;
+				}
+			}
+		}
+	}
+
+	virtual ~ProxyReadFile() { delete RF; }
+};
+
+
+int main() {
+	//ReadFile A("Readme.txt");
+	//A.PrintFile();
+	ProxyReadFile B("Winamp.txt");
+	B.PrintFile();
+
+	system("Pause");
+	return 0;
+}
---
+++ 12. Proxy/2.0 Document.cpp
@@ -0,0 +1,162 @@
+﻿#include <iostream>
+#include <memory>
+
+// https://www.badprog.com/c-design-pattern-proxy
+
+class IPage {
+public:
+	virtual ~IPage() {};
+	virtual void displayText() = 0;
+	virtual void displayImage() = 0;
+	virtual void displayVideo() = 0;
+	virtual void displayEmpty() = 0;
+};
+
+class Page : public IPage {
+public:
+	Page();
+	~Page();
+	void displayText() override;
+	void displayImage() override;
+	void displayVideo() override;
+	void displayEmpty() override;
+};
+
+Page::Page() {
+	std::cout << __FUNCTION__ << std::endl;
+}
+
+Page::~Page() {
+	std::cout << std::endl << __FUNCTION__ << std::endl;
+}
+
+void Page::displayText() {
+	std::cout << "Displaying text." << std::endl;
+}
+
+void Page::displayImage() {
+	std::cout << "Displaying an image." << std::endl;
+}
+
+void Page::displayVideo() {
+	std::cout << "Displaying a video." << std::endl;
+}
+
+void Page::displayEmpty() {
+	std::cout << "Sorry nothing to display." << std::endl;
+}
+
+class PageProxy : public IPage {
+public:
+	PageProxy();
+	~PageProxy();
+	void displayText() override;
+	void displayImage() override;
+	void displayVideo() override;
+	void displayEmpty() override;
+
+	void setCurrentPage(int currentPage);
+	const int getCurrentPage() const;
+	std::shared_ptr<Page> getPage();
+	void displayContent(int pageNumber);
+private:
+	int _currentPage;
+	std::shared_ptr<Page> _page;
+};
+
+PageProxy::PageProxy() {
+	std::cout << __FUNCTION__ << std::endl;
+	_page = NULL;
+}
+
+PageProxy::~PageProxy() {
+	std::cout << __FUNCTION__ << std::endl;
+}
+
+std::shared_ptr<Page> PageProxy::getPage() {
+	if (NULL == _page)
+		_page = std::make_shared<Page>();
+	return _page;
+}
+
+void PageProxy::setCurrentPage(int currentPage) {
+	_currentPage = currentPage;
+}
+
+const int PageProxy::getCurrentPage() const {
+	return _currentPage;
+}
+
+void PageProxy::displayContent(int pageNumber) {
+	setCurrentPage(pageNumber);
+	std::cout << "On page \"" << getCurrentPage();
+	std::cout << "\":" << std::endl;
+	switch (pageNumber)
+	{
+	case 1:
+		getPage()->displayText();
+		break;
+	case 24:
+		getPage()->displayText();
+		getPage()->displayImage();
+		break;
+	case 37:
+		getPage()->displayText();
+		getPage()->displayImage();
+		getPage()->displayVideo();
+		break;
+	default:
+		getPage()->displayEmpty();
+		break;
+	}
+	std::cout << std::endl;
+}
+
+void PageProxy::displayText() {}
+void PageProxy::displayImage() {}
+void PageProxy::displayVideo() {}
+void PageProxy::displayEmpty() {}
+
+class Document {
+public:
+	Document();
+	~Document();
+
+	void open(int page = 1);
+	void initProxy();
+	void changeToPage(int pageNumber);
+private:
+	PageProxy _proxy;
+};
+
+Document::Document() {
+	std::cout << __FUNCTION__ << std::endl;
+	initProxy();
+}
+
+Document::~Document() {
+	std::cout << __FUNCTION__ << std::endl;
+}
+
+void Document::open(int pageNumber) {
+	_proxy.displayContent(pageNumber);
+}
+
+void Document::initProxy() {}
+
+void Document::changeToPage(int pageNumber) {
+	_proxy.displayContent(pageNumber);
+}
+
+int main() {
+	Document doc;
+	doc.open(); // by default it opens page 1
+	doc.changeToPage(4);
+	doc.changeToPage(15);
+	doc.changeToPage(24);
+	doc.changeToPage(1);
+	doc.changeToPage(37);
+	doc.changeToPage(59);
+	system("Pause");
+	return 0;
+}
