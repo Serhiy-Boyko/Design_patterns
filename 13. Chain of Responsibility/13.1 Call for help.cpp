2eaeae2670ed1d9e5b130488f9929e00720e0675
Serhiy Boyko <Serhiy@ukr.net>
2019-11-24 23:59:52 +0200

    Chain_of_Responsibility

---
+++ 13. Chain of Responsibility/13.1 Call for help.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+#include <vector>
+#include <ctime>
+#include <string>
+using namespace std;
+
+class Base {
+	Base *next; // 1. Вказівник "next" у базовому класі
+public:
+	Base() : next(nullptr) {}
+
+	// приєднуємо наступну обробку тут або передаємо далі
+	void setNext(Base *n) { next ? next->setNext(n) : next = n; }
+
+	// 2. Метод базового класа, що делегує запит next-об'єкту
+	virtual void handle(int c) { next->handle(c); }
+	// c - рівень складності проблеми
+};
+
+class Operator : public Base {
+	string name;
+public:
+	Operator(string n) : name(n) {}
+
+	/*virtual*/ void handle(int c) {
+		cout << " Operator " << name << ": Can I help You ? " << endl;
+		if (c < 8) // якщо проблема проста, вирішуємо тут
+			cout << " Operator " << name << ": Problem " << c << " is solved ! " << endl;
+		else {// якщо надто складно, передаємо виклик спеціалісту
+			cout << " Operator " << name << ": Please, wait connection with specialist" << endl;
+			Base::handle(c); // 3. делегуємо виклик базовому класу
+		}
+	}
+};
+
+class Specialist : public Base {
+public:
+	/*virtual*/ void handle(int c) {
+		cout << " Specialist: Can I help You ? " << endl;
+		cout << " Specialist: Problem " << c << " is solved ! " << endl;
+	}
+};
+
+class Commutator : public Base {
+	vector <Operator*> Operators;
+public:
+	void add(Operator *o) { Operators.push_back(o); }
+
+	void setNext(Base *n) { for (auto it : Operators) it->setNext(n); }
+
+	/*virtual*/ void handle(int c) {
+		cout << " Commutator: please, wait connection with operator" << endl;
+		int cnt = Operators.size(); // к-ть операторів
+		switch (cnt) {
+		case 0: cout << " Commutator: sorry, no one of free operator" << endl; break;
+		case 1:	cout << " Your call will be save " << endl;
+			Operators[0]->handle(c);//3. делегуємо виклик базовому класу
+			break;
+		default:
+			cout << " Your call will be save " << endl;
+			Operators[rand() % cnt]->handle(c);//3. делегуємо виклик базовому класу
+		}
+	}
+};
+
+void main()
+{
+	srand(time(0));
+	Commutator c;
+	c.add(new Operator("Oleg"));
+	c.add(new Operator("Ira"));
+	c.add(new Operator("Misha"));
+	c.setNext(new Specialist);
+	for (int i = 1; i < 10; i++) {
+		c.handle(rand() % 10);
+		cout << endl;
+	}
+}
---
+++ 13. Chain of Responsibility/13.2 Call for help.cpp
@@ -0,0 +1,103 @@
+﻿#include <iostream>
+#include <vector>
+#include <ctime>
+#include <string>
+
+class Base {
+	Base *next; // 1. Вказівник "next" у базовому класі
+public:
+	Base() : next(nullptr) {}
+
+	// приєднуємо наступну обробку тут або передаємо далі
+	void setNext(Base *n) { 
+		if (next)
+			next->setNext(n);
+		else
+			next = n; 
+	}
+
+	// 2. Метод базового класа, що делегує запит next-об'єкту
+	// c - рівень складності проблеми
+	virtual void handle(int c) { next->handle(c); }	
+};
+
+class Employee : public Base {
+protected:
+	std::string name;
+public:
+	Employee(std::string n) : name(n) {}
+	virtual void handle(int c) = 0;
+};
+
+class Operator : public Employee {
+public:
+	Operator(std::string n) : Employee(n) {}
+
+	virtual void handle(int c) {
+		std::cout << " Operator " << name << ": Can I help You ? " << std::endl;
+		std::cout << " Client: My problem is " << c << "." << std::endl;
+		if (c < 8) // якщо проблема проста, вирішуємо тут
+			std::cout << " Operator " << name << ": Problem " << c << " is solved! \n";
+		else {// якщо надто складно, передаємо виклик спеціалісту
+			std::cout << " Operator " << name;
+			std::cout << ": Please, wait connection with specialist" << std::endl;
+			Base::handle(c); // 3. делегуємо виклик базовому класу
+		}
+	}
+};
+
+class Specialist : public Employee {
+public:
+	Specialist(std::string n) : Employee(n) {}
+
+	virtual void handle(int c) {
+		std::cout << " Specialist " << name << ": Can I help You ? " << std::endl;
+		std::cout << " Client: My problem is " << c << "." << std::endl;
+		std::cout << " Specialist " << name << ": Problem " << c << " is solved! \n";
+	}
+};
+
+class Commutator : public Base {
+	std::string employee;
+	std::vector <Employee*> Employees;
+public:
+	Commutator(std::string emp) : employee(emp) {}
+	void add(Employee *emp) { Employees.push_back(emp); }
+
+	void setNext(Base *n) { for (auto it : Employees) it->setNext(n); }
+
+	virtual void handle(int c) {
+		std::cout << " Commutator: Please, wait connection with ";
+		std::cout << employee << std::endl;
+		int cnt = Employees.size(); // к-ть операторів
+		switch (cnt) {
+		case 0: std::cout << " Commutator: Sorry, no one of free " << employee << '\n';
+			break;
+		case 1:	std::cout << " Your call will be save " << std::endl;
+			Employees[0]->handle(c);//3. делегуємо виклик базовому класу
+			break;
+		default:
+			std::cout << " Your call will be save " << std::endl;
+			Employees[rand() % cnt]->handle(c);//3. делегуємо виклик базовому класу
+		}
+	}
+};
+
+int main()
+{
+	srand(time(0));
+	Commutator comOperators("operator");
+	comOperators.add(new Operator("Oleg"));
+	comOperators.add(new Operator("Ira"));
+	comOperators.add(new Operator("Misha"));
+	Commutator *comSpecialists = new Commutator("specialist");
+	comSpecialists->add(new Specialist("Alex"));
+	comSpecialists->add(new Specialist("Ruslan"));
+	comOperators.setNext(comSpecialists);
+	for (int i = 1; i < 10; i++) {
+		comOperators.handle(rand() % 10);
+		std::cout << std::endl;
+	}
+	system("Pause");
+	return 0;
+}
