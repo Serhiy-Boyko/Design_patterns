e2c269d479994f73cd3db3c92f058f18c05fa5b9
Serhiy Boyko <Serhiy@ukr.net>
2019-12-02 23:32:55 +0200

    Observer

---
+++ 19. Observer/1. Search of offenders.cpp
@@ -0,0 +1,105 @@
+﻿/* Task 1. Реалізувати пошук злочинців на "Дикому Заході".
+Суддя видає ордери на арешт злочинців і розсилає їх шерифу, мисливцю за головами і маршалу. Шериф додає злочинця до списку розшукуваних, окрім «Біла чотири пальці», якого «кришує».  Мисливець і маршал можуть мати лише одну ціль пошуку, при чому маршал береться за будь-якого злочинця, якщо не зайнятий, а мисливець лише за тих, за кого дають понад 1000 доларів, при умові, що він вже не зайнятий іншим пошуком. */
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Offender { // правопорушник
+	std::string name;
+	int dangerous; // рівень небезпеки
+	int reward;    // винагорода
+public:
+	Offender(std::string n, int d, int r) 
+		: name(n), dangerous(d), reward(r) {}
+
+	void Print() {
+		std::cout << " Wanted: " << name;
+		std::cout << " Dangerous: " << dangerous;
+		std::cout << " Reward: " << reward << std::endl;
+	}
+
+	std::string getname() { return name; }
+	int getdangerous() { return dangerous; };
+	int getreward() { return reward; };
+};
+
+class Observer {
+public:
+	virtual void update(Offender *off) = 0;
+};
+
+/* Суддя (Subject), він оголошує злочинців в розшук
+і надсилає повідомлення потенційним пошукачам */
+class Judge {
+	std::vector<Offender*> Offenders; // список правопорушників
+	std::vector<Observer*> Views;     // список спостерігачів
+public:
+	void addObserver(Observer *obs) { Views.push_back(obs); }
+
+	void addOffender(Offender *off) {
+		Offenders.push_back(off);
+		std::cout << "------------------------" << std::endl;
+		std::cout << "Prosecutor:";
+		off->Print();
+		notify(off);
+	}
+
+	void notify(Offender *off) {
+		for (int i = 0; i < Views.size(); ++i)
+			Views[i]->update(off);
+	}
+};
+
+class Sheriff : public Observer {
+	std::vector<Offender*> targets;
+public:
+	Sheriff(Judge *model) { model->addObserver(this); }
+
+	/* virtual */ void update(Offender *off) {
+		if (off->getname() != "Bill four fingers") {// шериф його "кришує"
+			targets.push_back(off);
+			std::cout << " Sheriff: Understood! " << std::endl;
+		}
+	}
+};
+
+class Hunter : public Observer {
+	Offender *target;
+public:
+	Hunter(Judge *model) : target(nullptr) { model->addObserver(this); }
+
+	/* virtual */ void update(Offender *off) {
+		if (off->getreward() > 1000 && !target) {
+			target = off;
+			std::cout << " Hunter: I take him! " << std::endl;
+		}
+		else
+			std::cout << " Hunter: Not interesting! " << std::endl;
+	}
+};
+
+class Marshal : public Observer {
+	Offender *target;
+public:
+	Marshal(Judge *model) : target(nullptr) { model->addObserver(this); }
+
+	/* virtual */ void update(Offender *off) {
+		if (!target) {
+			target = off;
+			std::cout << " Marshal: Order is accepted! " << std::endl;
+		}
+	}
+};
+
+int main() {
+	Judge judge;
+	Sheriff s(&judge);
+	Hunter h(&judge);
+	Marshal m(&judge);
+
+	judge.addOffender(new Offender("Dirty Harry", 4, 700));
+	judge.addOffender(new Offender("Bill four fingers", 7, 2100));
+	judge.addOffender(new Offender("Mad Bart", 9, 3800));
+	system("pause");
+	return 0;
+}
---
+++ 19. Observer/2. Auction.cpp
@@ -0,0 +1,106 @@
+﻿/* Task 2. Реалізувати торги на аукціоні, де учасники щоразу піднімають ціну на визначений відсоток. */
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Lot{ // учасник аукціону
+	std::string name; 
+	size_t price;     // початкова ціна
+public:
+	Lot(std::string n, int p) : name(n), price(p) {}
+
+	void Print() {
+		std::cout << " Lot: " << name << " price: " << price << std::endl;
+	}
+
+	std::string getname() { return name; }
+	int getprice() { return price; };
+	void incprice() { price*=1.1; }
+};
+
+class Auctioneer;
+
+class Bidder { // учасник аукціону (Observer)
+protected:
+	std::string name;
+	size_t cash;
+	Auctioneer *auctioneer;
+public:
+	Bidder(std::string n, int c, Auctioneer *a);
+
+	void Print() {
+		std::cout << " Bidder: " << name << " cash: " << cash << std::endl;
+	}
+
+	std::string getname() { return name; }
+	size_t getcash() { return cash; };
+
+	virtual void update(Lot *l);
+};
+
+/* Ведучий торгів (Subject), оголошує лот і веде торги,
+   збільшуючи щоразу ціну на 10 % */
+class Auctioneer {
+	Lot* lot; // Лот
+	std::string name = ""; // ім'я учасника, що запропонував останню ціну
+	std::vector<Bidder*> Bidders; // список учасників торгів
+public:
+	void addObserver(Bidder *obs) { Bidders.push_back(obs); }
+
+	void takeLot(Lot *l) { lot = l; }
+
+	void offerLot() { // notify
+		std::cout << "-------------------------------------" << std::endl;
+		std::cout << "Auctioneer: ";
+		lot->Print();
+		for (int i = 0; i < Bidders.size(); ++i)
+			Bidders[i]->update(lot);
+	}
+
+	void takePropose(std::string n) { 
+		name = n;
+		lot->incprice(); 
+	}
+	size_t getprice() { return lot->getprice(); }
+	std::string getname() { return name; }
+
+	void Result() { 
+		std::cout << "======================================" << std::endl;
+		std::cout << "Auctioneer: congratulations the " << name << "!\n";
+		lot->Print();
+	}
+};
+
+Bidder::Bidder(std::string n, int c, Auctioneer *a)
+	: name(n), cash(c), auctioneer(a) 
+{
+	auctioneer->addObserver(this);
+}
+
+void Bidder::update(Lot *lot) {
+	if (auctioneer->getname() != name && lot->getprice()*1.2 < cash) {
+		std::cout << " Bidder: " << name << " increase! " << std::endl;
+		auctioneer->takePropose(name);
+	}
+}
+
+int main() {
+	Auctioneer auctioneer;
+	Bidder bidder1("Smith", 6000, &auctioneer);
+	Bidder bidder2("O'Nil", 8000, &auctioneer);
+	Bidder bidder3("McLaut", 12000, &auctioneer);
+
+	size_t price = 2000;
+	auctioneer.takeLot(new Lot("pen of Van Damme", price));
+	size_t nextprice = 0;
+	do {
+		if (nextprice > price)
+			price = nextprice;
+		auctioneer.offerLot();
+		nextprice = auctioneer.getprice();
+	} while (nextprice > price);
+
+	auctioneer.Result();
+	system("pause");
+	return 0;
+}
